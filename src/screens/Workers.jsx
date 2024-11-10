d259d0871c66aa399db6aa8241fc1dedaea379d6
Edson Ibarra <edsonibarra54@gmail.com>
2024-11-09 22:34:34 -0600

    Added thing to the workers screen

--- src/screens/Workers.jsx
+++ src/screens/Workers.jsx
@@ -1,8 +1,19 @@
 import React from "react";
+import './Workers.css';
 
 const Workers = () => {
-    return <div>
-        Workers
+    return <div class="workers">
+        <div class="title">Empleados en riesgo de rotacion</div>
+        <div>Empleados con su probabilidad de renuncia</div>
+        <div class="table">
+            <div class="header">
+                <div>Num</div>
+                <div>Nombre</div>
+                <div>Departamento</div>
+                <div>Riesgo</div>
+                <div>Ver más</div>
+            </div>
+        </div>
     </div>;
 };
 
