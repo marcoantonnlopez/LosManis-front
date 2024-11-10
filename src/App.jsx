aa5c630f9df18450a9394fd8436fdcde91f2c9fd
marcoantonnlopez <a305350@alumnos.uaslp.mx>
2024-11-09 22:17:39 -0600

    Resolved merge conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,24 @@ import Dashboard from "./screens/Dashboard.jsx";
 import Workers from "./screens/Workers.jsx";
 import NavBar from "./components/NavBar.jsx";
 import Home from "./screens/Home.jsx";
-<<<<<<< HEAD
-import Survey from "./screens/survey.jsx";
-=======
+import Survey from "./screens/Survey.jsx"; // Asegúrate de que la ruta sea correcta
 import Footer from "./components/Footer.jsx";
->>>>>>> ce9d0585fc29eb2ad8ce138647f2d04e2c2abbfc
 
 function App() {
-
   return (
     <>
-    <Router>
-      <NavBar></NavBar>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/dashboard" element={<Dashboard/>}/>
-        <Route path="/workers" element={<Workers/>}/>
-        <Route path="/EncuestaInduccion" element={<Survey/>}/>
-      </Routes>
-      <Footer></Footer>
-    </Router>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/workers" element={<Workers />} />
+          <Route path="/EncuestaInduccion" element={<Survey />} />
+        </Routes>
+        <Footer />
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
